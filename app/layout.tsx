48396a259fb5086be1b0de4d1cf39fcf6dd17ca5
VICTUS <miguelrin.morales@gmail.com>
2025-06-20 13:09:52 -0500

    Arreglo del fondo al hacer zoom en las imagenes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import 'nextra-theme-docs/style.css'
 import Image from 'next/image'
 import '../src/app/globals.css';
 
+
 export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
